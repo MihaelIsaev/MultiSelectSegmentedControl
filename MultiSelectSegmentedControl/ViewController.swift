a97790ecf2bd712991b365ecb863ee713ec21bdc
Mihael Isaev <isaev.mihael@gmail.com>
2017-03-12 20:41:08 +0400

    Initial Commit

---
+++ MultiSelectSegmentedControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiSelectSegmentedControl
+//
+//  Created by Mihael Isaev on 12/03/2017.
+//  Copyright © 2017 Mihael Isaev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
